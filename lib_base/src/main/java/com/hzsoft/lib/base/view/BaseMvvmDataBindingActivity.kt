82d5af39793cc27799ec85c7a2ccdc6aac63b65c
zhouxiang <zhou_xiang99@163.com>
2022-06-29 11:20:41 +0800

    新增基础库lib_base

---
+++ lib_base/src/main/java/com/hzsoft/lib/base/view/BaseMvvmDataBindingActivity.kt
@@ -0,0 +1,40 @@
+package com.hzsoft.lib.base.view
+
+import android.view.ViewStub
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.hzsoft.lib.base.mvvm.viewmodel.BaseViewModel
+
+/**
+ * Describe:
+ * 基础 DataBinding 页面
+ *
+ * @author zhou
+ * @Date 2020/12/17
+ */
+abstract class BaseMvvmDataBindingActivity<V : ViewDataBinding, VM : BaseViewModel> :
+    BaseMvvmActivity<VM>() {
+    protected lateinit var mBinding: V
+    private var viewModelId = 0
+
+    override fun initContentView(mViewStubContent: ViewStub) {
+        mViewStubContent.layoutResource = onBindLayout()
+        initViewDataBinding(mViewStubContent)
+        mViewStubContent.inflate()
+    }
+
+    private fun initViewDataBinding(mViewStubContent: ViewStub) {
+        viewModelId = onBindVariableId()
+        mViewStubContent.setOnInflateListener { _, inflated ->
+            mBinding = DataBindingUtil.bind<V>(inflated)!!
+            mBinding.setVariable(viewModelId, mViewModel)
+        }
+    }
+
+    abstract fun onBindVariableId(): Int
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding.unbind()
+    }
+}
